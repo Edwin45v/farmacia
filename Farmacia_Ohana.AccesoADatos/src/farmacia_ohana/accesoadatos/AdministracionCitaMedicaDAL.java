384c799f83a14c08f5ef0bfc55e108bd26cdd61e
Edwin Alejandro Galicia de la Quadra <edwin.galicia@agape.edu.sv>
2023-08-21 11:22:50 -0600

    Agrege las clases de Acceso a Datos

---
+++ Farmacia_Ohana.AccesoADatos/src/farmacia_ohana/accesoadatos/AdministracionCitaMedicaDAL.java
@@ -0,0 +1,11 @@
+
+package farmacia_ohana.accesoadatos;
+
+import java.util.*;
+import java.sql.*;
+import java.time.LocalDate;
+import farmaciaohana.entidadesdenegocio.*;
+
+public class AdministracionCitaMedicaDAL {
+    
+}
